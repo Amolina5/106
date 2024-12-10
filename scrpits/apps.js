18b1444a6eabfa3fad46f20df891efd68e42cc0a
Amolina5 <amolina555@hotmail.com>
2024-12-09 21:09:34 -0600

    assignmet three

--- scrpits/apps.js
+++ scrpits/apps.js
@@ -1,15 +1,70 @@
 function saveTask()
 {
-    console.log(`saving task`);
+    console.log('saving task');
+    //get values
+    const title = $("#txtTitle").val();
+    const descript = $("#txtDescription").val();
+    const color = $("#selColor").val();
+    const date = $("#selDate").val();
+    const status = $("#selStatus").val();
+    const budget = $("#numBudget").val();
+    console.log(title,descript,color,date,status,budget);
+    //build an object
+    let taskToSave = new Task(title, descript, color, date, status, budget);
+    console.log(taskToSave);
+
+
+//save to sever
+$.ajax({
+    tyoe:"POST",
+    url: "http://fsdiapi.azurewebsites.net/api/tasks/",
+    date: JSON.stringify(taskToSave),
+    contentType: "application/json",
+    success: function(response) {
+    console.log(response);
+
+    },
+    error: function(error){
+        console.log(error);
+    }
+    
+});
+//display task
+displayTask(taskToSave)
 }
 
-function init() {
-    console.log("task manger");
-    //load data
+function displayTask(task)
+{
+    console.log(task);
+    let syntax = `<div class="task">
+    <h5>${task.title}</h5>
+        </div>
+        <div><label>${task.status}</label></div>
+        <div><label>${task.date}</label>
+        <label>${task.budget}</label></div>`
+        ;
+
+        $(".list").append(syntax);
+}
+
+function testRequest(){
+    $.ajax({
+        type: "get",
+        url: "http://fsdiapi.azurewebsites.net",
+        success: function(response){
+            console.log(response);
+        },
+        error: function(y)
+        {
+            console.log(y)
+        }
+        })
+    }
+function init(){
+    console.log("task manager");
+    // load data
 
     //hook the events
     $("#btnSave").click(saveTask);
-
 }
-
 window.onload = init;
