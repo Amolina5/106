66c5ea3797194fd90cc18c8a31646277b63a2a2e
Amolina5 <amolina555@hotmail.com>
2024-12-11 12:56:56 -0600

    final report 106

--- scrpits/apps.js
+++ scrpits/apps.js
@@ -13,24 +13,22 @@ function saveTask()
     let taskToSave = new Task(title, descript, color, date, status, budget);
     console.log(taskToSave);
 
+    //save to server
+    $.ajax({
+        type: "POST",
+        url: "http://fsdiapi.azurewebsites.net/api/tasks/",
+        data: JSON.stringify(taskToSave),
+        contentType: "application/json",
+        success: function(response) {
+        console.log(response);
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
 
-//save to sever
-$.ajax({
-    tyoe:"POST",
-    url: "http://fsdiapi.azurewebsites.net/api/tasks/",
-    date: JSON.stringify(taskToSave),
-    contentType: "application/json",
-    success: function(response) {
-    console.log(response);
-
-    },
-    error: function(error){
-        console.log(error);
-    }
-    
-});
-//display task
-displayTask(taskToSave)
+    //display the task
+    //displayTask(taskToSave);
 }
 
 function displayTask(task)
@@ -38,33 +36,64 @@ function displayTask(task)
     console.log(task);
     let syntax = `<div class="task">
     <h5>${task.title}</h5>
+    <p>${task.description}</p>
         </div>
         <div><label>${task.status}</label></div>
         <div><label>${task.date}</label>
-        <label>${task.budget}</label></div>`
+        <label>${task.budget}</label></div>`        
         ;
 
-        $(".list").append(syntax);
+    $(".list").append(syntax);
 }
 
 function testRequest(){
     $.ajax({
         type: "get",
-        url: "http://fsdiapi.azurewebsites.net",
+        url: "http://fsdiapi.azurewebsites.net/", 
         success: function(response){
             console.log(response);
         },
-        error: function(y)
+        error: function(error)
         {
-            console.log(y)
+            console.log(error)
         }
-        })
-    }
-function init(){
+                
+    });
+}
+
+function loadTask(){
+$.ajax({
+    type: "get",
+    url: "http://fsdiapi.azurewebsites.net/api/tasks",
+
+    success:function(response){
+        console.log(response);
+        let data = JSON.parse(response);
+        for(let i= 0; i<data.length;i++){
+            let task = data[i];
+            if (task.name === "AlexMolina"){
+                displayTask(task);
+            }
+        }
+        console.log(data);
+    },
+    error:function(error){
+        console.log(error);
+    }    
+})
+}
+// from the object returned, get only messages that were created by you.
+// tip 1. Modify the class to include your user.
+// tip 2. use a for loop
+// tip 3. Then an if statement
+
+function init() {
     console.log("task manager");
-    // load data
+    //load data
+    loadTask();
 
     //hook the events
     $("#btnSave").click(saveTask);
 }
+
 window.onload = init;
